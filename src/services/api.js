2782bdd9ee4d036567683e9b3fcd96b0d7995105
FlaviaRamosdaSilva <didi.flavia@gmail.com>
2024-02-19 11:58:20 -0300

    alteração do endereço da API

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 // conforme documentação do axios
 
 const apiCodeBurger = axios.create({
-  baseURL: 'https://sushi-api.onrender.com/' // endereço do backend
+  baseURL: 'sushi-api.up.railway.app/' // endereço do backend
 })
 
 apiCodeBurger.interceptors.request.use(async config => {
